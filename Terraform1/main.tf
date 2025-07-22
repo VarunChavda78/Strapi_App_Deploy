b229572ffcd0971f7727785aa029dfb2c2b0df3b
VarunChavda78 <varunchavda078@gmail.com>
2025-07-22 14:14:49 +0530

    Update terraform.yml file

--- Terraform1/main.tf
+++ Terraform1/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-app-sg"
+  name        = "strapi-app-seeg"
   description = "Allow SSH and Strapi"
 
   ingress {
