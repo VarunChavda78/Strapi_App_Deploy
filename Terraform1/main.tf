5784dc4cd2602f2b144e693b748e371d4da607f2
VarunChavda78 <varunchavda078@gmail.com>
2025-07-23 11:59:42 +0530

    update sec grp

--- Terraform1/main.tf
+++ Terraform1/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-appvcv-seecg"
+  name        = "strapi-appsecsec-seecg"
   description = "Allow SSH and Strapi"
 
   ingress {
